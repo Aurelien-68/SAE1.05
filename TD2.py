40c33c45cc891e7bb8135804829e5ea16c498829
e2400575 <aurelienhenry9@gmail.com>
2025-01-14 11:32:55 +0100

    f

--- TD2.py
+++ TD2.py
@@ -1,12 +1,14 @@
 import csv
 etudiants = [
-{"ID": 1, "Nom": "Schmitt", "Prénom": "Albert", "Note": 14},
-{"ID": 2, "Nom": "Al-Hakim", "Prénom": "Yasmine", "Note": 17},
-{"ID": 3, "Nom": "Tran", "Prénom": "Sebastien", "Note": 12},
-{"ID": 4, "Nom": "Meyer", "Prénom": "Claire", "Note": 16},
-{"ID": 5, "Nom": "Kobayashi", "Prénom": "Kaito", "Note": 11}
+{"ID": 1, "Nom": "Schmitt", "Prenom": "Albert", "Note": 14},
+{"ID": 2, "Nom": "Al-Hakim", "Prenom": "Yasmine", "Note": 17},
+{"ID": 3, "Nom": "Tran", "Prenom": "Sebastien", "Note": 12},
+{"ID": 4, "Nom": "Meyer", "Prenom": "Claire", "Note": 16},
+{"ID": 5, "Nom": "Kobayashi", "Prenom": "Kaito", "Note": 11}
 ]
 
 with open('donnees.csv', mode='w', encoding='utf-8', newline='') as file:
-    donnees = csv.writer(file)
+    champs = ["ID","Nom", "Prenom", "Note"]
+    donnees = csv.DictWriter(file, fieldnames=champs)
+    donnees.writeheader()
     donnees.writerows(etudiants)
