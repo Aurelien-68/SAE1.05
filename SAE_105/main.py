ea514376768db0498ceb32fd6a379e0bf0fe3c73
e2400575 <aurelienhenry9@gmail.com>
2025-01-16 10:19:25 +0100

    d

--- SAE_105/main.py
+++ SAE_105/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 def liste_fichier():
     # Crée un objet Path pour le répertoire de base
-    repertoire_de_base = Path()
+    repertoire_de_base = Path.cwd()
 
     # Liste pour stocker les informations sur les fichiers
     infos_fichiers=[]
@@ -12,7 +12,9 @@ def liste_fichier():
         chemin_fichier = Path(fichier)
         nom_fichier = fichier
         taille_fichier = chemin_fichier.stat().st_size  # Taille en octets
-        infos_fichiers.append(chemin_fichier, nom_fichier, taille_fichier)
+        infos_fichiers.append([chemin_fichier, nom_fichier, taille_fichier])
+    return infos_fichiers
 
 
-    print(infos_fichiers)
+infos = liste_fichier()
+print(infos)
