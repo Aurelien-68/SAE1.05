0e731d169d7f13f0c58b203a8153772f518c9cc4
e2400575 <aurelienhenry9@gmail.com>
2025-01-16 10:39:29 +0100

    d

--- SAE_105/main.py
+++ SAE_105/main.py
@@ -9,13 +9,24 @@ def liste_fichier():
 
     # Liste les fichiers et sous-répertoires du répertoire et leurs taille en octets
     for fichier in repertoire_de_base.iterdir():
-        if fichier.is_file():  # Assurez-vous qu'il s'agit d'un fichier et pas d'un répertoire
+        if fichier.is_file():  # pour s'assurer que c'est bien un fichier et pas un repertoire
             chemin_fichier = str(fichier)  # Convertir le chemin en chaîne de caractères
             nom_fichier = fichier.name  # Extraire le nom du fichier sans son chemin
             taille_fichier = fichier.stat().st_size  # Taille en octets
-            infos_fichiers.append([chemin_fichier, nom_fichier, f"{taille_fichier} octets"])
+            infos_fichiers.append([chemin_fichier, nom_fichier, taille_fichier])
             return infos_fichiers
 
+# Trie la liste en fonction de la taille (le troisième élément), de manière décroissante
+def trier_par_taille(infos_fichiers):
+    # Trier la liste par la taille
+    return sorted(infos_fichiers, key=lambda x: x[2], reverse=True)
 
+# Récupérer les info des fichiers
 infos = liste_fichier()
-print(infos)
+
+# Trier les informations par taille
+infos_triees = trier_par_taille(infos)
+
+# Afficher les résultats triés
+for info in infos_triees:
+    print(f"Chemin : {info[0]}, Nom : {info[1]}, Taille : {info[2]} octets")
