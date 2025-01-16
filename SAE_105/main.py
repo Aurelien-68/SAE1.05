af1b5e52b9e4ce8dda9e1594be8e47f8fbc948d3
e2400575 <aurelienhenry9@gmail.com>
2025-01-16 09:09:52 +0100

    d

--- SAE_105/main.py
+++ SAE_105/main.py
@@ -1,2 +1,6 @@
 def repertoire_de_base:
-    
+    # Crée un objet Path pour le répertoire de base
+    chemin_base = Path(repertoire)
+
+    # Liste pour stocker les informations sur les fichiers
+    infos_fichier=[]
